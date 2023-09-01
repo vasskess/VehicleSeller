ef1b8657f61836d5d02f4a4cd5d7216c1415b24e
Vasil <vrangelov85@abv.bg>
2023-09-01 16:04:45 +0300

    added tests for user patch method & one more file separation

--- tests/accounts/test_private_api_functionality/test_users_functionality.py
+++ tests/accounts/test_private_api_functionality/test_users_functionality.py
@@ -4,6 +4,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from tests.helpers.account_test_helpers import (
     create_user,
+    test_user,
     USER_DETAILS_URL,
 )
 
@@ -72,7 +73,7 @@ class PrivateAccountApiTests(TestCase):
         self.assertEqual(result.data["password"][0], expected_error)
         self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_put_method_with_empy__credential_fields_returns_proper_error_messages_and_proper_status_code(
+    def test_put_method_with_empty_credential_fields_returns_proper_error_messages_and_proper_status_code(
         self,
     ):
         updated_credentials = {}
@@ -80,7 +81,6 @@ class PrivateAccountApiTests(TestCase):
         result = self.client.put(USER_DETAILS_URL, updated_credentials)
 
         expected_error = "This field is required."
-        print(result.data["password"])
 
         self.assertEqual(result.data["email"][0], expected_error)
         self.assertEqual(result.data["password"][0], expected_error)
@@ -144,3 +144,116 @@ class PrivateAccountApiTests(TestCase):
 
         self.assertEqual(result.data["password"][0], expected_error_message)
         self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_patch_method_with_valid_user_credentials_update_user_credentials_and_returns_proper_status_code(
+        self,
+    ):
+        updated_credentials = {
+            "email": "new_secret_email@mhmm.com",
+            "password": "newpasswordmhmm",
+        }
+
+        result = self.client.patch(USER_DETAILS_URL, updated_credentials)
+
+        self.assertEqual(self.user.email, updated_credentials["email"])
+        self.assertTrue(self.user.check_password(updated_credentials["password"]))
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+
+    def test_patch_method_with_valid_email_change_email_successfully_and_returns_proper_status_code(
+        self,
+    ):
+        updated_credentials = {
+            "email": "new_secret_email@mhmm.com",
+        }
+
+        result = self.client.patch(USER_DETAILS_URL, updated_credentials)
+
+        self.assertEqual(self.user.email, updated_credentials["email"])
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+
+    def test_patch_method_with_valid_password_change_password_successfully_and_returns_proper_status_code(
+        self,
+    ):
+        updated_credentials = {
+            "password": "newpasswordmhmm",
+        }
+
+        result = self.client.patch(USER_DETAILS_URL, updated_credentials)
+
+        self.assertTrue(self.user.check_password(updated_credentials["password"]))
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+
+    def test_patch_method_with_invalid_email_returns_proper_error_message_and_proper_status_code(
+        self,
+    ):
+        updated_credentials = {
+            "email": "new_secret_email@",
+        }
+
+        result = self.client.patch(USER_DETAILS_URL, updated_credentials)
+
+        self.assertEqual(result.data["email"][0], EmailValidator.message)
+        self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_patch_method_with_short_password_returning_proper_error_message_and_proper_status_code(
+        self,
+    ):
+        updated_credentials = {
+            "password": "1234567",
+        }
+
+        result = self.client.patch(USER_DETAILS_URL, updated_credentials)
+
+        expected_error_message = (
+            "This password is too short. It must contain at least 8 characters."
+        )
+
+        self.assertEqual(result.data["password"][0], expected_error_message)
+        self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_patch_method_with_common_password_returning_proper_error_message_and_proper_status_code(
+        self,
+    ):
+        updated_credentials = {
+            "password": "qwerty123",
+        }
+
+        result = self.client.patch(USER_DETAILS_URL, updated_credentials)
+        expected_error_message = "This password is too common."
+
+        self.assertEqual(result.data["password"][0], expected_error_message)
+        self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_patch_method_with_entirely_numeric_password_returning_proper_error_message_and_proper_status_code(
+        self,
+    ):
+        updated_credentials = {
+            "password": "6589012345",
+        }
+
+        result = self.client.patch(USER_DETAILS_URL, updated_credentials)
+
+        expected_error_message = "This password is entirely numeric."
+
+        self.assertEqual(result.data["password"][0], expected_error_message)
+        self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_patch_method_with_empty_credential_fields_doesnt_change_credentials_and_returns_proper_status_code(
+        self,
+    ):
+        initial_password = self.user.password
+
+        updated_credentials = {}
+
+        result = self.client.patch(USER_DETAILS_URL, updated_credentials)
+
+        updated_user = test_user.objects.get(id=self.user.id)
+
+        self.assertEqual(result.data["email"], self.user.email)
+        self.assertEqual(updated_user.password, initial_password)
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+
+    def test_post_method_on_user_details_endpoint_is_not_allowed(self):
+        result = self.client.post(USER_DETAILS_URL, {})
+
+        self.assertEqual(result.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
